f5a981133ede6d100509f010dacf145cb86aa452
Diego <pcdiego17@gmail.com>
2026-04-14 14:11:48 -0600

    agregando enlaces de redes sociales

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const EMAIL = "mejiadiego17@gmail.com";
-export const LINKEDIN = "https://www.linkedin.com/in/mejiadiego17/";
-export const GITHUB = "https://github.com/diegomejia17";
+export const LINKEDIN = "https://www.linkedin.com/in/diegomejia17/";
+export const GITHUB = "https://github.com/diegomejia17/";
